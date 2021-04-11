fff1647b9462265f5b02e637b2c01a7d71af0a2d
TalyssonG <lopestalysson99@gmail.com>
2021-04-11 16:58:34 -0300

    Criando o servidor

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const server = express()
+
+//request, response 
+
+server.get('/', (request, response) => {
+  return response.send('OI!!') //Send: Enviar
+})
+
+server.listen(3000, () => console.log('rodando 🚀'))
