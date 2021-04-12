61149f7592aba923c4f71635c1c4defaa1c3d6b2
TalyssonG <lopestalysson99@gmail.com>
2021-04-12 15:39:47 -0300

    Instalando o nodemon

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require("express")
 const server = express()
 
 //request, response 
-
 server.get('/', (request, response) => {
-  return response.send('OI!!') //Send: Enviar
+  return response.send('OI!') //Send: Enviar
 })
+
 
 server.listen(3000, () => console.log('rodando 🚀'))
