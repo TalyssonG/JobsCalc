a4756ac625ef255c930c5a4e415a7f83b0ff10a4
TalyssonG <lopestalysson99@gmail.com>
2021-04-14 15:19:42 -0300

    Componentização

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,23 @@ const express = require('express'); //express: Biblioteca para criar o servidor
 const routes = express.Router() //Vai criar as rotas
 
 const views = __dirname + "/views/"
+
+const profile = {
+  name:  "Talysson",
+  avatar: "https://avatars.githubusercontent.com/u/68451890?v=4",
+  "monthly-budget": 6000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4,
+}
+
 //req, res 
 routes.get('/', (req, res) => res.render(views + "index")) 
 routes.get('/job', (req, res) => res.render(views + "job")) 
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit")) 
-routes.get('/profile', (req, res) => res.render(views + "profile")) 
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile })) 
+
+
 
 
 module.exports = routes;
